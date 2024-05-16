2320d18bc00e382ebb66e9ba2e91ebf44d495c43
gasaroleila <uwamgaleila@gmail.com>
2024-05-16 10:36:25 +0200

    linting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,7 +21,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password, password)
 
-
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_email = [
@@ -35,20 +34,17 @@ class ModelTests(TestCase):
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
 
-
     def test_new_user_without_email_raises_error(self):
         """Creating a user without email raises an exception"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
 
-
     def test_create_superuser(self):
         """Test creating a superuser"""
         user = get_user_model().objects.create_superuser(
             'test@example.com',
-            'test123' 
+            'test123'
         )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-
--- app/user/schema.py
+++ app/user/schema.py
@@ -5,8 +5,10 @@ User Graphql schema
 import graphene
 from graphql_auth.schema import UserQuery, MeQuery
 
+
 class Query(UserQuery, MeQuery, graphene.ObjectType):
     """User Query"""
     pass
+
 
 schema = graphene.Schema(query=Query)
